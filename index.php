fdedc29e4e9f233d129c0dccb539262b8749a62a
ggbandit <ggbandit@hotmail.com>
2018-07-04 15:12:03 +0700

    Chart.JS 04-07-2018

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
   <!-- Latest compiled and minified CSS -->
 
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
   <!-- Optional theme -->
 
 
@@ -73,10 +73,50 @@
         </li>
       </ul>
     </div>
-    <img src="graph.jpg" class="img-fluid mx-auto d-block">
+    <canvas id="myChart" width="120vw" height="40vh"></canvas>
+<script>
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 15, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
 
   </div>
   <!-- Graph -->
+  
   <!-- button -->
   <div class="container-fluid my-4">
     <!-- Button to Open the Modal -->
